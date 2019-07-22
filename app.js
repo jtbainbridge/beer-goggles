48c472af0cfeb7702f929bebcc5fe0f5b769ef70
James Bainbridge <jamestbainbridge@icloud.com>
2019-07-22 15:26:26 +0100

    Globalise API request function for acc and grid

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let data = []; //Store raw API data here
+
 // Get the div, accordion-root for dynamic content
 const app = document.getElementById('accordion-root');
 // create a container div
@@ -11,60 +13,71 @@ app.appendChild(container);
 const request = new XMLHttpRequest();
 request.open('GET', 'https://api.punkapi.com/v2/beers/', true);
 request.onload = function () {
-  // Store api object in a variable 'data'
-  const data = JSON.parse(this.response);
+  // Store API object in a variable 'data'
+  data = JSON.parse(this.response);
   // Check status of request
   if (request.status >= 200 && request.status < 400) {
-    data.forEach(beer => {
-      // 1. Create a div with a card class
-      const card = document.createElement('div');
-      card.setAttribute('class', 'accItem');
-
-      // 2. Create a button for the beer title
-      const accBtn = document.createElement('button');
-      accBtn.setAttribute('class', 'accordion');
-      accBtn.textContent = beer.name;
-
-      // 3. Create a div for the more info content
-      const accInfo = document.createElement('div');
-      accInfo.setAttribute('class', 'accordion-content');
-
-      // 4. Create the content items (description, brewed date, abv, img)
-      const p1 = document.createElement('p');
-      p1.textContent = beer.description;
-      // Add brewed date
-      const p2 = document.createElement('p');
-      p2.textContent = `First brewed in ${beer.first_brewed}`;
-      // Add ABV value
-      const p3 = document.createElement('p');
-      p3.textContent = `ABV ${beer.abv}%`;
-      // Add image
-      const imgSpan = document.createElement('span');
-      const imgTag = document.createElement('img');
-      imgTag.setAttribute("src", beer.image_url);
-      imgSpan.appendChild(imgTag);
-
-      // 5. Append the items from (4.) to the div in (3.)
-      accInfo.appendChild(imgSpan);
-      accInfo.appendChild(p1);
-      accInfo.appendChild(p2);
-      accInfo.appendChild(p3);
-
-      // Each card will contain:
-      card.appendChild(accBtn);
-      card.appendChild(accInfo);
-
-      // Append the cards to the container element
-      container.appendChild(card);
-    })
+    // call initial setup function
+    setup(data)
   } else {
     document.getElementById('accordion-container').innerHTML = 'Sorry there has been an issue trying to fetch the data. Please try again later.'
     console.log('Error fetching data. Check URL or server status.')
   }
+}
+request.send();
+
+// Initial setup
+function setup(para) {
+  buildAccordion(para); //Build Accordion
+}
+
+// Build Accordion
+function buildAccordion(para) {
+  para.forEach(beer => {
+    // 1. Create a div with a card class
+    const card = document.createElement('div');
+    card.setAttribute('class', 'accItem');
+
+    // 2. Create a button for the beer title
+    const accBtn = document.createElement('button');
+    accBtn.setAttribute('class', 'accordion');
+    accBtn.textContent = beer.name;
+
+    // 3. Create a div for the more info content
+    const accInfo = document.createElement('div');
+    accInfo.setAttribute('class', 'accordion-content');
+
+    // 4. Create the content items (description, brewed date, abv, img)
+    const p1 = document.createElement('p');
+    p1.textContent = beer.description;
+    // Add brewed date
+    const p2 = document.createElement('p');
+    p2.textContent = `First brewed in ${beer.first_brewed}`;
+    // Add ABV value
+    const p3 = document.createElement('p');
+    p3.textContent = `ABV ${beer.abv}%`;
+    // Add image
+    const imgSpan = document.createElement('span');
+    const imgTag = document.createElement('img');
+    imgTag.setAttribute("src", beer.image_url);
+    imgSpan.appendChild(imgTag);
+
+    // 5. Append the items from (4.) to the div in (3.)
+    accInfo.appendChild(imgSpan);
+    accInfo.appendChild(p1);
+    accInfo.appendChild(p2);
+    accInfo.appendChild(p3);
+
+    // Each card will contain:
+    card.appendChild(accBtn);
+    card.appendChild(accInfo);
+
+    // Append the cards to the container element
+    container.appendChild(card);
+  })
   accordion();
   closeAll();
 }
-request.send();
 
 // ACCORDION ACTIONS
 function accordion() {
@@ -90,10 +103,10 @@ function closeAll() {
   const closeBtn = document.getElementById('close-btn');
   const accs = document.querySelectorAll('.accordion');
   closeBtn.addEventListener('click', function (event) {
-      for (let i = 0; i < accs.length; i++) {
-          accs[i].classList.remove('is-open');
-          const content = accs[i].nextElementSibling;
-          content.style.maxHeight = null;
-      }
+    for (let i = 0; i < accs.length; i++) {
+      accs[i].classList.remove('is-open');
+      const content = accs[i].nextElementSibling;
+      content.style.maxHeight = null;
+    }
   });
 }
