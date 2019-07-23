c4f77a2bee52bf5e3af5fdf9685001fec684cce9
James Bainbridge <jamestbainbridge@icloud.com>
2019-07-23 16:13:48 +0100

    extracted carousel options into own module

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import { buildAccordion } from './modules/accordion.js';
 import { buildGrid, refreshGrid } from './modules/grid.js';
+import { slider1, slider2 } from './modules/carousel.js';
 
 let data = []; //Store raw API data here
 let sorted = []; //Store sorted API data
@@ -129,40 +130,3 @@ reset.addEventListener('click', function () {
   buildGrid(sorted);
   console.log('reset button hit');
 })
-
-
-// SLIDER OPTIONS
-const slider1 = tns({
-  container: '.my-slider1',
-  items: 1,
-  slideBy: 'page',
-  mouseDrag: true,
-  swipeAngle: false,
-  speed: 400,
-  navPosition: 'bottom',
-  arrowKeys: true
-});
-
-const slider2 = tns({
-  container: '.my-slider2',
-  items: 3,
-  slideBy: 'page',
-  edgePadding: 10,
-  controls: false,
-  mouseDrag: true,
-  swipeAngle: false,
-  speed: 400,
-  navPosition: 'bottom',
-  responsive: {
-      "350": {
-          items: 2,
-          controls: false
-      },
-      "500": {
-          items: 3
-      },
-      "1024": {
-          items: 4
-      }
-  }
-});
---
+++ modules/carousel.js
@@ -0,0 +1,37 @@
+// SLIDER OPTIONS
+const slider1 = tns({
+  container: '.my-slider1',
+  items: 1,
+  slideBy: 'page',
+  mouseDrag: true,
+  swipeAngle: false,
+  speed: 400,
+  navPosition: 'bottom',
+  arrowKeys: true
+});
+
+const slider2 = tns({
+  container: '.my-slider2',
+  items: 3,
+  slideBy: 'page',
+  edgePadding: 10,
+  controls: false,
+  mouseDrag: true,
+  swipeAngle: false,
+  speed: 400,
+  navPosition: 'bottom',
+  responsive: {
+      "350": {
+          items: 2,
+          controls: false
+      },
+      "500": {
+          items: 3
+      },
+      "1024": {
+          items: 4
+      }
+  }
+});
+
+export { slider1, slider2 }
